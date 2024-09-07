b7aa9bd021a99b7a5e43f473837beadeaf521a81
Ridley Angeles <161462260+readss04@users.noreply.github.com>
2024-09-07 16:19:07 +0800

    Add files via upload

---
+++ signin(students).php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/Event Management System/css files/signinstud.css">
+  <title>Eventify - Sign In Page - Students</title>
+  
+</head>
+<body>
+    <div class="dark-green"></div>
+
+    <div class="signin-container">
+      <div class="sigin-separation">
+        <div class="signin-description">
+          <h1 class="signin-title">Eventify</h1>
+          <p class="sigin-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vero sapiente labore.</p>
+        </div>
+
+        <div class="signin-description1">
+          <h2 class="title">Sign In</h2>
+          <input class="signin-studnumber" type="email" placeholder="Email Address">
+          <input class="signin-password" type="password" placeholder="Password">
+          <a href="/Event Management System/events.php" target="_self">Sign In</a>
+          <div class="sigin-images-align">
+            <img class="signin-images" src="/Event Management System/images/gc-logo.png" alt="gc-logo">
+            <img class="signin-images" src="/Event Management System/images/gc-ccs.png" alt="gc-logo-css">
+          </div>
+        </div>
+      </div>
+      <img class="sigin-vectorpng" src="/Event Management System/images/undraw_sign_in_re_o58h.svg" alt="image vector">
+
+    </div>
+</body>
+</html>
