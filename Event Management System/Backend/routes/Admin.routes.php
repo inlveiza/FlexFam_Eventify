3e84f9d13c9aa2d97e712babb51703b6bba8f77b
Kyu020 <aaronjanestacio20@gmail.com>
2024-11-16 11:40:18 +0800

    hdnwu

--- Event Management System/Backend/routes/Admin.routes.php
+++ Event Management System/Backend/routes/Admin.routes.php
@@ -1,5 +1,5 @@
 <?php
-	if($req[0] = 'addevent'){
+	if($req[0] == 'addevent'){
 		$response = $admin->AddEvent($data_input);
         echo json_encode($response);
 	}
--- Event Management System/Backend/routes/Auth.routes.php
+++ Event Management System/Backend/routes/Auth.routes.php
@@ -1,15 +1,15 @@
 <?php
-       if($req[0] = 'login'){
+       if($req[0] == 'login'){
     	   $response = $auth->login($data_input);
            echo json_encode($response);
        }
        
-       if($req[0] = 'signup'){
+       if($req[0] == 'signup'){
        	$response = $auth->register($data_input);
            echo json_encode($response);
        }
        
-       if($req[0] = 'logout'){
+       if($req[0] == 'logout'){
        	$response = $auth->logout();
            echo json_encode($response);
        }
